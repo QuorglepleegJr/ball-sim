2ee9189fe5cc856492c52105c0e6ec9f84735b40
Benjamin <17WilsB@olab.org.uk>
2023-02-08 21:26:32 +0000

    v0.1 - Created window, wrote baseline framework for program

---
+++ main.py
@@ -0,0 +1,21 @@
+# Kivy Imports
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+# Other Imports
+
+from sys import exit
+
+# Widgets
+
+# Applications
+
+class SimulationApp(App):
+
+    pass
+
+# Main
+
+if __name__ == "__main__":
+    exit(SimulationApp().run())
